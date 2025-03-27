2cab08da8305a5d063d51e23be931aab55da334b
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-03-27 07:56:00 -0700

    db transfered successfully to supabase

--- config/db.js
+++ config/db.js
@@ -1,14 +1,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+// console.log("DATABASE_URL from env:", process.env.DATABASE_URL);
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST,
     dialect: 'postgres',
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   }
+    // }
   }
 );
 
 module.exports = sequelize;
+
+// import postgres from 'postgres'
+
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
+
+// export default sql
